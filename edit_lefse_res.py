a1352bc9c07f29c5d1545dc8cdadf3cd971d9cd4
jiali0411 <jyu28@vols.utk.edu>
2020-07-16 10:59:51 -0400

    python script to edit lefse

---
+++ edit_lefse_res.py
@@ -0,0 +1,22 @@
+"""
+Create on July 16, 2020
+Author: Jiali
+This is the script to edit the lefse res file. Make the taxonomy levels separate by '|'. 
+Usage: python edit_lefse_res.py <input res file> <output file name>
+"""
+
+import sys
+import re
+
+file_in = sys.argv[1]
+file_out = sys.argv[2]
+
+with open(file_in,"r") as f, open(file_out,"w") as out:
+    for line in f:
+        # first we remove the level before Bacteria
+        remove_D0 = line.strip("D_0__")
+        # next we replace the D_x__ by pipe
+        edit_ID = re.sub(r'D_\d__', '|', remove_D0)
+        # then remove all leftover _
+        new_ID = edit_ID.replace('_','')
+        out.write(new_ID)
